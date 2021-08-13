e33cee5fdae8848cead992ff94e9cfdc5ae8046e
LibertusDio <51104238+LibertusDio@users.noreply.github.com>
2021-08-13 12:54:05 +0700

    ADD: StructToMap

---
+++ util.go
@@ -0,0 +1,12 @@
+package toolbox
+
+func StructToMap(b interface{}) (map[string]interface{}, error) {
+	data, err := json.Marshal(b)
+	if err != nil {
+		return nil, err
+	}
+
+	m := make(map[string]interface{})
+	err = json.Unmarshal(data, &m) // Convert to a map
+	return m, err
+}
